87bf25aaf0d266ea098acb697a2338c8fdc7c8c9
Shivam Chavan <144063863+shivam-chavan-05@users.noreply.github.com>
2025-02-11 17:06:58 -0500

    Create 141.py

---
+++ 141.py
@@ -0,0 +1,15 @@
+#Linked List Cycle
+
+#Sol 1
+class Solution:
+    def hasCycle(self, head: Optional[ListNode]) -> bool:
+        visited = set()
+        while head:
+            if head in visited:
+                return True
+            visited.add(head)
+            head = head.next
+        return False
+
+
+#Sol 2
