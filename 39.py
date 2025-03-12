221c4efe88b84c6abfca80627fbd358a11d36c67
Shivam Chavan <144063863+shivam-chavan-05@users.noreply.github.com>
2025-03-12 18:26:55 -0400

    Create 39.py

---
+++ 39.py
@@ -0,0 +1,23 @@
+# Combination Sum
+
+class Solution:
+    def combinationSum(self, candidates: List[int], target: int) -> List[List[int]]:
+        res, sol = [], []
+        n = len(candidates)
+
+        def backtrack(i, cur_sum):
+            if cur_sum == target:
+                res.append(sol[:])
+                return
+
+            if cur_sum > target or i == n:
+                return
+
+            backtrack(i+1, cur_sum)
+
+            sol.append(candidates[i])
+            backtrack(i, cur_sum + candidates[i])
+            sol.pop()
+
+        backtrack(0, 0)
+        return res
